d099618b7997683b1a3c45bcd5d2f6eed7321487
HugoMathus <hugomathus.a@gmail.com>
2024-08-01 22:36:07 -0700

    Carousel y Head
    Agregué un carousel de fotos y también mejore el head

--- public/JavaScript/script.js
+++ public/JavaScript/script.js
@@ -4,8 +4,41 @@ function openNav() {
     document.getElementById("main").style.marginLeft = "250px";
   }
   
-  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-  }
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
+var slideIndex = 0;
+showSlides(slideIndex);
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n >= slides.length) {slideIndex = 0}
+    if (n < 0) {slideIndex = slides.length - 1}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex].style.display = "block";
+    dots[slideIndex].className += " active";
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    clearInterval(autoSlide);
+    showSlides(slideIndex = n - 1);
+    autoSlide = setInterval(function() { plusSlides(1); }, 5000);
+}
+
+var autoSlide = setInterval(function() {
+    plusSlides(1);
+}, 5000);
