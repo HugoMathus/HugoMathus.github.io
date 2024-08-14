00302a8c7d4f4551bac785a65817b0aca4d2e222
HugoMathus <hugomathus.a@gmail.com>
2024-08-13 23:26:30 -0700

    Añadimos footer
    Añadimos footer y además los menús

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Ruta principal para servir el archivo index.html
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'build.html'));
 });
 
 app.listen(puerto, () => {
